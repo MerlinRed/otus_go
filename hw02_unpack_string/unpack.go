987ccd4b80d6d9e88abef97fa323e1e2e5af43ac
Iliya Soldatov <i.a.soldatov@tinkoff.ru>
2023-12-29 17:24:53 +0500

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,6 +9,33 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
+func checkCornerCases(value string, packedString string, index int) (string, error) {
+	firstAlpha := packedString[0]
+	lastAlpha := packedString[len(packedString)-1]
+
+	if string(lastAlpha) == value {
+		if _, err := strconv.Atoi(string(packedString[index-1])); err == nil {
+			return "", ErrInvalidString
+		}
+	}
+
+	if string(firstAlpha) == value {
+		if _, err := strconv.Atoi(string(packedString[index+1])); err == nil {
+			return "", ErrInvalidString
+		}
+	}
+
+	if _, err := strconv.Atoi(string(packedString[index+1])); err == nil {
+		return "", ErrInvalidString
+	}
+
+	if _, err := strconv.Atoi(string(packedString[index-1])); err == nil {
+		return "", ErrInvalidString
+	}
+
+	return value, nil
+}
+
 func Unpack(packedString string) (string, error) {
 	if len(packedString) == 0 {
 		return packedString, nil
@@ -18,60 +45,35 @@ func Unpack(packedString string) (string, error) {
 		return packedString, nil
 	}
 
-	firstAlpha := packedString[0]
-	lastAlpha := packedString[len(packedString)-1]
-
-	if _, err := strconv.Atoi(string(firstAlpha)); err == nil {
-		return "", ErrInvalidString
-
-	}
-
 	var builder strings.Builder
 	var alpha string
 
 	for index, value := range strings.Split(packedString, "") {
 		if _, err := strconv.Atoi(value); err == nil {
-			if string(lastAlpha) == value {
-				if _, err := strconv.Atoi(string(packedString[index-1])); err == nil {
-					return "", ErrInvalidString
-				}
+			if _, err := checkCornerCases(value, packedString, index); err != nil {
+				return "", err
 			}
-
-			if string(firstAlpha) == value {
-				if _, err := strconv.Atoi(string(packedString[index+1])); err == nil {
-					return "", ErrInvalidString
-				}
-			}
-
-			if _, err := strconv.Atoi(string(packedString[index+1])); err == nil {
-				return "", ErrInvalidString
-			}
-
-			if _, err := strconv.Atoi(string(packedString[index-1])); err == nil {
-				return "", ErrInvalidString
-			}
-
 		}
 
 		if digit, err := strconv.Atoi(value); err == nil && digit != 0 {
-			digit -= 1
+			digit--
 			builder.WriteString(strings.Repeat(alpha, digit))
-		} else {
-			if num, e := strconv.Atoi(value); num == 0 && e == nil {
+			continue
+		}
+
+		if num, e := strconv.Atoi(value); num == 0 && e == nil {
+			continue
+		}
+
+		lastAlpha := packedString[len(packedString)-1]
+		if string(lastAlpha) != value && string(lastAlpha) != "0" {
+			if beforeAlpha := packedString[index+1]; string(beforeAlpha) == "0" {
 				continue
 			}
-
-			lastAplha := packedString[len(packedString)-1]
-			if string(lastAplha) != value && string(lastAplha) != "0" {
-				if beforeAlpha := packedString[index+1]; string(beforeAlpha) == "0" {
-					continue
-				}
-			}
-
-			alpha = value
-			builder.WriteString(strings.Repeat(alpha, 1))
-
 		}
+
+		alpha = value
+		builder.WriteString(strings.Repeat(alpha, 1))
 	}
 
 	fmt.Println(builder.String())
