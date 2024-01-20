b2e35800ec5af869b2ecc3ab5cc7b35a320c68e8
Iliya Soldatov <i.a.soldatov@tinkoff.ru>
2024-01-20 23:31:02 +0500

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,65 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type Dictionary struct {
+	Key    int
+	Values []string
+}
+
+func counter(stringArray []string) map[string]int {
+	counter := make(map[string]int)
+	for _, word := range stringArray {
+		counter[word]++
+	}
+
+	return counter
+}
+
+func groupByCount(counter map[string]int) map[int][]string {
+	intCounter := make(map[int][]string, len(counter))
+	for word, count := range counter {
+		intCounter[count] = append(intCounter[count], word)
+	}
+
+	return intCounter
+}
+
+func getSortedArrayDict(counter map[int][]string) []Dictionary {
+	arrayDict := make([]Dictionary, 0, len(counter))
+	for count, words := range counter {
+		arrayDict = append(arrayDict, Dictionary{count, words})
+	}
+	sort.Slice(arrayDict, func(i, j int) bool {
+		return arrayDict[i].Key > arrayDict[j].Key
+	})
+
+	return arrayDict
+}
+
+func getMostCommonWords(arrayDict []Dictionary) []string {
+	var mostCommon []string
+	count := 0
+	for _, dict := range arrayDict {
+		if count >= 10 {
+			break
+		}
+
+		sort.Strings(dict.Values)
+		count += len(dict.Values)
+		mostCommon = append(mostCommon, dict.Values...)
+	}
+
+	return mostCommon
+}
+
+func Top10(text string) []string {
+	counter := counter(strings.Fields(text))
+	intCounter := groupByCount(counter)
+	arrayDict := getSortedArrayDict(intCounter)
+
+	return getMostCommonWords(arrayDict)
 }
